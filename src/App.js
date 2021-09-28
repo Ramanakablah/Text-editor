125a009ee7134fc1b5ff3322b6878ff922c69e54
Raman <alexesramon0909@gmail.com>
2021-09-28 19:41:09 +0530

    React app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
+import Navbar from './components/Navbar'
 import './App.css';
-
+import PropTypes from '../node_modules/prop-types'
+import Textform from './components/Textform';
+import React, { useState } from 'react'
+import Alert from './components/Alert';
+// import About from './components/About';
+// import {
+//   BrowserRouter as Router,
+//   Switch,
+//   Route,
+//   Link
+// } from "react-router-dom";
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+  const [mode, setmode] = useState('light')
+  const [alerte, setalerte] = useState(null)
+  const showalert = (msg, typ) => {
+    setalerte({
+      message: msg,
+      type: typ
+    })
+    setTimeout(() => {
+      setalerte(null)
+    }, 1500)
+  }
+  const ChangeMode = () => {
+    if (mode === 'light') {
+      document.body.style.backgroundColor = '#001E5B'
+      setmode('dark');
+      showalert("Dark mode on", "Success")
+      document.title = 'Text Editor in Dark mode'
+    }
+    else {
+      setmode('light');
+      showalert("Light mode on", "Success")
+      document.body.style.backgroundColor = 'white'
+      document.title = 'Text Editor in Light mode'
+    }
+  }
+
+  return (
+    <>
+      <Navbar mode={mode} ChangeMode={ChangeMode} />
+      <Alert alert={alerte} />
+      <Textform mode={mode} showalert={showalert} />
+      </>
+      );
+  }
+
+      Navbar.propTypes= {
+        first : PropTypes.string
+  }
+
+      export default App;

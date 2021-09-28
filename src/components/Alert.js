4735d439b0eac2d8539cdff81eb708891557ed63
Raman <alexesramon0909@gmail.com>
2021-09-28 19:48:26 +0530

    Updates

--- src/components/Alert.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function alert(props) {
-    return (
-        props.alert && <div className="alert alert-warning alert-dismissible fade show" role="alert">
-            <strong>{props.alert.message}</strong>
-        </div>
-    )
-}
